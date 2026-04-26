94a6d69c2ecb513b81abed016a3e851662943604
John <147444612+OfforJohn@users.noreply.github.com>
2026-04-26 11:25:58 -0700

    Initial commit from Create Twenty App

---
+++ src/constants/universal-identifiers.ts
@@ -0,0 +1,4 @@
+export const APP_DISPLAY_NAME = 'Pivora';
+export const APP_DESCRIPTION = 'cold calling';
+export const APPLICATION_UNIVERSAL_IDENTIFIER = 'cd701823-b3b5-4bdc-908c-aadaa901cc70';
+export const DEFAULT_ROLE_UNIVERSAL_IDENTIFIER = '5ba5bd52-6fe4-4fa3-a9c5-0293ce6d43aa';
